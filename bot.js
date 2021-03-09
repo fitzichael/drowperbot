97c270a603e3ce0ee1508b78fbcaebfee16882eb
Michael Fitzgerald <fitzichael@gmail.com>
2021-03-08 20:22:25 -0500

    base functionality

---
+++ bot.js
@@ -0,0 +1,19 @@
+const auth = require("./auth.json");
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('pootypie dreamily ❤️❤️❤️', {type: "WATCHING"}); 
+});
+
+client.on('message', (msg) => {
+  if (msg.author === "348915387708080128") {
+    if (msg.toString().contains('master')){
+      msg.channel.send("please notice me pooty");
+    }
+  }
+});
+
+client.login(auth.token);
